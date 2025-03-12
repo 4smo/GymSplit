f1f720fa75d53cb58f1c413cdaf5e17e5db0a866
asmo <asmo.koivula@gmail.com>
2025-03-12 15:31:28 +0200

    Barebone registering and login functionality

---
+++ app.py
@@ -0,0 +1,49 @@
+import sqlite3
+from flask import Flask
+from flask import redirect, render_template, request, session
+from werkzeug.security import generate_password_hash
+import db, users, config
+
+app = Flask(__name__)
+app.secret_key = config.secret_key
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/register")
+def register():
+    return render_template("register.html")
+
+@app.route("/create", methods=["POST"])
+def create():
+    username = request.form["username"]
+    password1 = request.form["password1"]
+    password2 = request.form["password2"]
+    if password1 != password2:
+        return "ERROR: passwords do not match"
+    password_hash = generate_password_hash(password1)
+
+    try:
+        sql = "INSERT INTO users (username, password_hash) VALUES (?, ?)"
+        db.execute(sql, [username, password_hash])
+    except sqlite3.IntegrityError:
+        return "ERROR: username taken"
+
+    return "Registeration successful"
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "GET":
+        return render_template("login.html")
+    
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+
+        user_id = users.check_login(username, password)
+        if user_id:
+            session["user_id"] = user_id
+            return redirect("/")
+        else:
+            return "ERROR: wrong username or password"
